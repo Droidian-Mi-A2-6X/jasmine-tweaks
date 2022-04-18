c4eeedb8de6f462246992da796ed331c214bf1b9
gibcheesepuffs <shou@disroot.org>
2022-04-19 02:58:28 +0530

    waydroid-setup: Use bookworm branch

--- waydroid-setup.sh
+++ waydroid-setup.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt install curl ca-certificates -y
 
 # Setting up waydroid repo
 
-export DISTRO="bullseye" && sudo curl -# --proto '=https' --tlsv1.2 -Sf https://repo.waydro.id/waydroid.gpg --output /usr/share/keyrings/waydroid.gpg && echo "deb [signed-by=/usr/share/keyrings/waydroid.gpg] https://repo.waydro.id/ $DISTRO main" > ~/waydroid.list && sudo mv ~/waydroid.list /etc/apt/sources.list.d/waydroid.list && echo "Updating system..." && sudo apt update && sudo apt dist-upgrade -y
+export DISTRO="bookworm" && sudo curl -# --proto '=https' --tlsv1.2 -Sf https://repo.waydro.id/waydroid.gpg --output /usr/share/keyrings/waydroid.gpg && echo "deb [signed-by=/usr/share/keyrings/waydroid.gpg] https://repo.waydro.id/ $DISTRO main" > ~/waydroid.list && sudo mv ~/waydroid.list /etc/apt/sources.list.d/waydroid.list && echo "Updating system..." && sudo apt update && sudo apt dist-upgrade -y
 
 echo "Installing waydroid package ..."
 
