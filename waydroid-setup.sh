09faf95710612fcfee9153b11e1f21d5e27bf9d7
gibcheesepuffs <shou@disroot.org>
2022-04-19 02:07:50 +0530

    waydroid-setup: Install curl and ca-certificates

--- waydroid-setup.sh
+++ waydroid-setup.sh
@@ -2,6 +2,8 @@
 
 echo "Instructions copied from https://docs.waydro.id/usage/install-on-desktops"
 
+sudo apt update && sudo apt install curl ca-certificates -y
+
 # Setting up waydroid repo
 
 export DISTRO="bullseye" && sudo curl -# --proto '=https' --tlsv1.2 -Sf https://repo.waydro.id/waydroid.gpg --output /usr/share/keyrings/waydroid.gpg && echo "deb [signed-by=/usr/share/keyrings/waydroid.gpg] https://repo.waydro.id/ $DISTRO main" > ~/waydroid.list && sudo mv ~/waydroid.list /etc/apt/sources.list.d/waydroid.list && echo "Updating system..." && sudo apt update && sudo apt dist-upgrade -y
